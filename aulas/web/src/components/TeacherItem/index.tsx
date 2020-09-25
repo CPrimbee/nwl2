80351c40da7c4dd57f26691a582ccc91c6a7a7d1
CPrimbee <cprimbee@gmail.com>
2020-09-25 15:51:08 -0300

    Finish component PageHeader

---
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./style.css";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars2.githubusercontent.com/u/40774358?s=460&u=59ef9f97b14cf461bccda80fd9af7086ef9a600e&v=4"
+          alt="Cristiano Gonçalves"
+        />
+        <div>
+          <strong>Cristiano Gonçalves</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 20,00 </strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
--- aulas/web/src/pages/TeacherList/index.tsx
+++ aulas/web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 
 import "./style.css";
+import TeacherItem from "../../components/TeacherItem";
 
 function TeacherList() {
   return (
@@ -10,18 +11,26 @@ function TeacherList() {
         <form id="search-teachers">
           <div className="input-block">
             <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
+            <input type="text" id="subject" />
           </div>
           <div className="input-block">
             <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
+            <input type="text" id="week_day" />
           </div>
           <div className="input-block">
             <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
+            <input type="text" id="time" />
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
     </div>
   );
 }
