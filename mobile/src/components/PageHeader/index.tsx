fadf743bf6c7034bfdc0d05ee006efecfba2957b
CPrimbee <cprimbee@gmail.com>
2020-10-28 08:14:18 -0300

    filter on teacher list

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,5 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { ReactNode } from "react";
 import { Image, View, Text } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 
@@ -7,14 +6,20 @@ import backIcon from "../../assets/images/icons/back.png";
 import logoImg from "../../assets/images/logo.png";
 
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  headerRight,
+  children,
+}) => {
   const { navigate } = useNavigation();
-  
+
   function handleGoBack() {
     navigate("Landing");
   }
@@ -29,7 +34,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImg} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   );
 };
